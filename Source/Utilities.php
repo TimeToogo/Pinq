b67bfa7c0b772971818a401637dd37ca71ebd050
Elliot Levin <elliot@aanet.com.au>
2014-04-12 11:56:12 +1000

    Remove unused utility methods

--- Source/Utilities.php
+++ Source/Utilities.php
@@ -7,36 +7,6 @@ DEFINE('IS_PHP_55', version_compare(PHP_VERSION, '5.5', '>='));
 final class Utilities
 {
     private function __construct() { }
-
-    public static $Identical = [__CLASS__, 'Identical'];
-
-    public static function Identical($One, $Two)
-    {
-        return $One === $Two;
-    }
-
-    public static $Comparison = [__CLASS__, 'Compare'];
-
-    public static function Compare($One, $Two)
-    {
-        if($One === $Two || $One == $Two) {
-            return 0;
-        }
-        
-        return $One > $Two ? 1 : -1;
-    }
-
-    public static function IsTransitive(array $Array)
-    {
-        foreach($Array as $Value) {
-            if(is_string($Value) || is_integer($Value)) {
-                continue;
-            }
-            return false;
-        }
-        
-        return true;
-    }
     
     public static function GetTypeOrClass($Value)
     {
