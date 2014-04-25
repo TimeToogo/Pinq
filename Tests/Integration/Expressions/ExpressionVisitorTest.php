ba2d72789d754814b0dff56fb088c703abf04456
Elliot Levin <elliot@aanet.com.au>
2014-04-25 23:38:45 +1000

    Fix expression visitor test method casing

--- Tests/Integration/Expressions/ExpressionVisitorTest.php
+++ Tests/Integration/Expressions/ExpressionVisitorTest.php
@@ -9,46 +9,46 @@ class ExpressionVisitorTest extends ExpressionTest
     public function expressionsToVisit()
     {
         return [
-            [O\Expression::arrayExpression([], []), 'VisitArray'],
+            [O\Expression::arrayExpression([], []), 'visitArray'],
             [O\Expression::assign(
                     O\Expression::value(0),
                     O\Operators\Assignment::EQUAL,
-                    O\Expression::value(0)), 'VisitAssignment'],
+                    O\Expression::value(0)), 'visitAssignment'],
             [O\Expression::binaryOperation(
                     O\Expression::value(0),
                     O\Operators\Binary::ADDITION,
-                    O\Expression::value(0)), 'VisitBinaryOperation'],
+                    O\Expression::value(0)), 'visitBinaryOperation'],
             [O\Expression::unaryOperation(
                     O\Operators\Unary::PLUS,
-                    O\Expression::value(0)), 'VisitUnaryOperation'],
-            [O\Expression::cast(O\Operators\Cast::STRING, O\Expression::value(0)), 'VisitCast'],
-            [O\Expression::closure([], [], []), 'VisitClosure'],
-            [O\Expression::emptyExpression(O\Expression::value(0)), 'VisitEmpty'],
-            [O\Expression::field(O\Expression::value(0), O\Expression::value(0)), 'VisitField'],
-            [O\Expression::functionCall(O\Expression::value(0)), 'VisitFunctionCall'],
-            [O\Expression::index(O\Expression::value(0), O\Expression::value(0)), 'VisitIndex'],
-            [O\Expression::invocation(O\Expression::value(0)), 'VisitInvocation'],
-            [O\Expression::issetExpression([O\Expression::value(0)]), 'VisitIsset'],
+                    O\Expression::value(0)), 'visitUnaryOperation'],
+            [O\Expression::cast(O\Operators\Cast::STRING, O\Expression::value(0)), 'visitCast'],
+            [O\Expression::closure([], [], []), 'visitClosure'],
+            [O\Expression::emptyExpression(O\Expression::value(0)), 'visitEmpty'],
+            [O\Expression::field(O\Expression::value(0), O\Expression::value(0)), 'visitField'],
+            [O\Expression::functionCall(O\Expression::value(0)), 'visitFunctionCall'],
+            [O\Expression::index(O\Expression::value(0), O\Expression::value(0)), 'visitIndex'],
+            [O\Expression::invocation(O\Expression::value(0)), 'visitInvocation'],
+            [O\Expression::issetExpression([O\Expression::value(0)]), 'visitIsset'],
             [O\Expression::methodCall(
                     O\Expression::value(0),
-                    O\Expression::value(0)), 'VisitMethodCall'],
-            [O\Expression::newExpression(O\Expression::value(0)), 'VisitNew'],
-            [O\Expression::parameter(''), 'VisitParameter'],
-            [O\Expression::returnExpression(), 'VisitReturn'],
+                    O\Expression::value(0)), 'visitMethodCall'],
+            [O\Expression::newExpression(O\Expression::value(0)), 'visitNew'],
+            [O\Expression::parameter(''), 'visitParameter'],
+            [O\Expression::returnExpression(), 'visitReturn'],
             [O\Expression::staticMethodCall(
                     O\Expression::value(0),
-                    O\Expression::value(0)), 'VisitStaticMethodCall'],
+                    O\Expression::value(0)), 'visitStaticMethodCall'],
             [O\Expression::subQuery(
                     O\Expression::value(0),
                     $this->getMock('\\Pinq\\Queries\\IRequestQuery'),
-                    O\Expression::invocation(O\Expression::value(0))), 'VisitSubQuery'],
+                    O\Expression::invocation(O\Expression::value(0))), 'visitSubQuery'],
             [O\Expression::ternary(
                     O\Expression::value(0),
                     null,
-                    O\Expression::value(0)), 'VisitTernary'],
-            [O\Expression::throwExpression(O\Expression::value(0)), 'VisitThrow'],
-            [O\Expression::value(0), 'VisitValue'],
-            [O\Expression::variable(O\Expression::value(0)), 'VisitVariable']
+                    O\Expression::value(0)), 'visitTernary'],
+            [O\Expression::throwExpression(O\Expression::value(0)), 'visitThrow'],
+            [O\Expression::value(0), 'visitValue'],
+            [O\Expression::variable(O\Expression::value(0)), 'visitVariable']
         ];
     }
 
@@ -64,7 +64,7 @@ class ExpressionVisitorTest extends ExpressionTest
                 ->expects($this->once())
                 ->method($method)
                 ->with($this->equalTo($expression));
-
+        
         $expressionVisitorMock->walk($expression);
     }
 }
