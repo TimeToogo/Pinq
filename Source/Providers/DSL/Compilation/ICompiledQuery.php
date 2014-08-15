7f0ce050cad0491300fc53b5f166834b5558bcee
Elliot Levin <elliot@aanet.com.au>
2014-08-15 17:05:59 +1000

    Update compiled query docblock

--- Source/Providers/DSL/Compilation/ICompiledQuery.php
+++ Source/Providers/DSL/Compilation/ICompiledQuery.php
@@ -5,8 +5,8 @@ namespace Pinq\Providers\DSL\Compilation;
 /**
  * Base interface for a compiled request / operation query.
  *
- * The compiled query is a parameterized query ready to be executed
- * against a set of parameters..
+ * The compiled query is a parametrized query ready to be executed
+ * against a set of parameters.
  *
  * @author Elliot Levin <elliotlevin@hotmail.com>
  */
