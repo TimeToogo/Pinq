a52e8cabd470e72f69f04b76da5eaa5238f80407
Elliot Levin <elliot@aanet.com.au>
2014-09-01 18:16:30 +1000

    Disallow closures that are representative of functions/methods (from \ReflectionFunction::getClosure or \ReflectionMethod::getClosure) and add explicit tests

--- Source/Parsing/Reflection.php
+++ Source/Parsing/Reflection.php
@@ -2,6 +2,8 @@
 
 namespace Pinq\Parsing;
 
+use Pinq\PinqException;
+
 /**
  * Utility class for getting the reflection from any type
  * of callable.
@@ -22,13 +24,36 @@ final class Reflection
      */
     final public static function fromCallable(callable $function)
     {
+        // If is array it could be an instance or static method:
+        // ['class', 'method'] or [$instance, 'method'];
         if (is_array($function)) {
             return new \ReflectionMethod($function[0], $function[1]);
-        } elseif ($function instanceof \Closure) {
-            return new \ReflectionFunction($function);
-        } elseif (is_object($function)) {
+        }
+        // If it is a closure it could be an actual closure or
+        // possibly a closure of another function from
+        // \ReflectionFunction::getClosure or \ReflectionMethod::getClosure
+        elseif ($function instanceof \Closure) {
+            $reflection = new \ReflectionFunction($function);
+
+            // If the name is {closure} it as an actual closure
+            if($reflection->getShortName() === '{closure}') {
+                return $reflection;
+            }
+            // Bail out, no (sane) way of determining the actual function
+            // represented by the closure
+            throw InvalidFunctionException::invalidFunctionMessage(
+                    'The function has been wrapped in closure '
+                    . '(most likely  via ReflectionFunction::getClosure or \ReflectionMethod::getClosure) '
+                    . 'and this is not supported',
+                    $reflection);
+        }
+        // If an object but not a closure it must be an object defining
+        // the __invoke magic method.
+        elseif (is_object($function)) {
             return new \ReflectionMethod($function, '__invoke');
-        } else {
+        }
+        // Fallback to function
+        else {
             $name = null;
             is_callable($function, false, $name);
 
---
+++ Tests/Integration/Parsing/ReflectionHelperTest.php
@@ -0,0 +1,160 @@
+<?php
+
+namespace Pinq\Tests\Integration\Parsing;
+
+use Pinq\Expressions as O;
+use Pinq\Parsing\Reflection;
+use Pinq\Tests\PinqTestCase;
+
+function funcHelper()
+{
+
+}
+
+class ReflectionHelperTest extends PinqTestCase
+{
+    protected $closureStartLine;
+    protected $closureEndLine;
+
+    protected function getClosure()
+    {
+        $this->closureStartLine = __LINE__; $closure = function () {
+
+        }; $this->closureEndLine = __LINE__;
+
+        return $closure;
+    }
+
+    protected function assertIsClosureReflection(\ReflectionFunctionAbstract $reflection)
+    {
+        $this->getClosure();
+        $this->assertInstanceOf('ReflectionFunction', $reflection);
+        /** @var $reflection \ReflectionFunction */
+        $this->assertTrue($reflection->isClosure());
+        $this->assertFalse($reflection->isInternal());
+        $this->assertSame(__CLASS__, $reflection->getClosureScopeClass()->getName());
+        $this->assertSame($this, $reflection->getClosureThis());
+        $this->assertSame(__NAMESPACE__, $reflection->getNamespaceName());
+        $this->assertSame('{closure}', $reflection->getShortName());
+        $this->assertSame($this->closureStartLine, $reflection->getStartLine());
+        $this->assertSame($this->closureEndLine, $reflection->getEndLine());
+    }
+
+    public function testClosure()
+    {
+        $reflection = Reflection::fromCallable($this->getClosure());
+
+        $this->assertIsClosureReflection($reflection);
+    }
+
+    public function testClosureAfterReflectionGetClosure()
+    {
+        $asNewClosure = (new \ReflectionFunction($this->getClosure()))->getClosure()->bindTo($this, __CLASS__);
+        $reflection = Reflection::fromCallable($asNewClosure);
+
+        $this->assertIsClosureReflection($reflection);
+    }
+
+    public function method()
+    {
+
+    }
+
+    protected function assertIsMethodReflection(\ReflectionFunctionAbstract $reflection)
+    {
+        $this->assertInstanceOf('ReflectionMethod', $reflection);
+        /** @var $reflection \ReflectionMethod */
+        $this->assertFalse($reflection->isClosure());
+        $this->assertFalse($reflection->isInternal());
+        $this->assertSame(null, $reflection->getClosureScopeClass());
+        $this->assertSame(__CLASS__, $reflection->getDeclaringClass()->getName());
+        $this->assertSame(null, $reflection->getClosureThis());
+        $this->assertSame('', $reflection->getNamespaceName());
+        $this->assertSame('method', $reflection->getShortName());
+        $this->assertSame('method', $reflection->getName());
+    }
+
+    public function testMethod()
+    {
+        $reflection = Reflection::fromCallable([$this, 'method']);
+        $this->assertIsMethodReflection($reflection);
+    }
+
+    /**
+     * @expectedException \Pinq\Parsing\InvalidFunctionException
+     */
+    public function testMethodAfterReflectionGetClosure()
+    {
+        $asClosure = (new \ReflectionMethod(__CLASS__, 'method'))->getClosure($this);
+        $reflection = Reflection::fromCallable($asClosure);
+
+        $this->assertIsMethodReflection($reflection);
+    }
+
+    public static function staticMethod()
+    {
+
+    }
+
+    protected function assertIsStaticMethodReflection(\ReflectionFunctionAbstract $reflection)
+    {
+        $this->assertInstanceOf('ReflectionMethod', $reflection);
+        /** @var $reflection \ReflectionMethod */
+        $this->assertFalse($reflection->isClosure());
+        $this->assertFalse($reflection->isInternal());
+        $this->assertSame(null, $reflection->getClosureScopeClass());
+        $this->assertSame(__CLASS__, $reflection->getDeclaringClass()->getName());
+        $this->assertSame(null, $reflection->getClosureThis());
+        $this->assertSame('', $reflection->getNamespaceName());
+        $this->assertSame('staticMethod', $reflection->getShortName());
+        $this->assertSame('staticMethod', $reflection->getName());
+    }
+
+    public function testStaticMethod()
+    {
+        $reflection = Reflection::fromCallable([$this, 'staticMethod']);
+        $this->assertIsStaticMethodReflection($reflection);
+    }
+
+    /**
+     * @expectedException \Pinq\Parsing\InvalidFunctionException
+     */
+    public function testStaticMethodAfterReflectionGetClosure()
+    {
+        $asClosure = (new \ReflectionMethod(__CLASS__, 'staticMethod'))->getClosure(null);
+        $reflection = Reflection::fromCallable($asClosure);
+
+        $this->assertIsStaticMethodReflection($reflection);
+    }
+
+    protected function assertIsFunctionReflection(\ReflectionFunctionAbstract $reflection)
+    {
+        $this->assertInstanceOf('ReflectionFunction', $reflection);
+        /** @var $reflection \ReflectionFunction */
+        $this->assertFalse($reflection->isClosure());
+        $this->assertFalse($reflection->isInternal());
+        $this->assertSame(null, $reflection->getClosureScopeClass());
+        $this->assertSame(null, $reflection->getClosureThis());
+        $this->assertSame(__NAMESPACE__, $reflection->getNamespaceName());
+        $this->assertSame('funcHelper', $reflection->getShortName());
+        $this->assertSame(__NAMESPACE__ . '\\funcHelper', $reflection->getName());
+    }
+
+    public function testFunction()
+    {
+        $reflection = Reflection::fromCallable(__NAMESPACE__ . '\\funcHelper');
+
+        $this->assertIsFunctionReflection($reflection);
+    }
+
+    /**
+     * @expectedException \Pinq\Parsing\InvalidFunctionException
+     */
+    public function testFunctionAfterReflectionGetClosure()
+    {
+        $asClosure = (new \ReflectionFunction(__NAMESPACE__ . '\\funcHelper'))->getClosure();
+        $reflection = Reflection::fromCallable($asClosure);
+
+        $this->assertIsFunctionReflection($reflection);
+    }
+}
